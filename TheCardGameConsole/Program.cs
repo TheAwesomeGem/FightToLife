ab78f458493832fa2c1122f1c6ab6409a06200be
TheAwesomeGem <theawesomegem@gmail.com>
2020-03-09 16:06:08 -0500

    * Added TODO

--- TheCardGameConsole/Program.cs
+++ TheCardGameConsole/Program.cs
@@ -2,6 +2,11 @@
 {
     internal static class Primary
     {
+        /**
+         * Implement a Module System where Game manages the module and each module can depend on Game and fetch module
+         * GameCommand can use Modules to have logic.
+         * Modules are like Service Container
+         */
         public static void Main()
         {
             ConsoleApplication application = new ConsoleApplication();
